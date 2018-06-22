c91e390a62782a05dd526b68fd14b5544a863dfb
Mugenor <lizaklokova@mail.ru>
2018-06-22 17:54:38 +0300

    new file

--- src/main/java/main/repositories/PositionRepository.java
+++
@@ -1,9 +0,0 @@
-package main.repositories;
-
-import main.entities.Position;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PositionRepository extends CrudRepository<Position, Integer>{
-}
--- src/main/java/main/repositories/SkillRepository.java
+++
@@ -1,9 +0,0 @@
-package main.repositories;
-
-import main.entities.Skill;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SkillRepository extends CrudRepository<Skill, Integer> {
-}
